ea66ad75c1b173066662b9634f3fd4d6526030f6
SouravBanerje <sourav2473486@gmail.com>
2024-12-28 09:43:25 +0530

    Initial commit

---
+++ app/src/main/java/com/example/explicitintentsvideo/Activity3.java
@@ -0,0 +1,62 @@
+package com.example.explicitintentsvideo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Activity3 extends AppCompatActivity {
+
+    EditText etSurname;
+    Button btnSubmit,btnCancel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_3);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        etSurname = findViewById(R.id.etSurname);
+        btnSubmit = findViewById(R.id.btnSubmit);
+        btnCancel = findViewById(R.id.btnCancel);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(etSurname.getText().toString().isEmpty()){
+                    Toast.makeText(Activity3.this,"Please enter all fields!",Toast.LENGTH_SHORT).show();
+
+                }
+                else{
+                    String surname = etSurname.getText().toString().trim();
+                    Intent intent = new Intent();
+                    intent.putExtra("surname",surname);
+                    setResult(RESULT_OK,intent);
+                    Activity3.this.finish();
+                }
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResult(RESULT_CANCELED);
+                Activity3.this.finish();
+            }
+        });
+    }
+}
